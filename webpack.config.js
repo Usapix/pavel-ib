d9c42a58922a1e5ef2d20291d4c5ccdd9f887c6d
Whittun <whittun@gmail.com>
2023-05-31 16:31:27 +0300

    feat: add startup scripts and basic settings

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  mode: "development",
+  devServer: {
+    compress: true,
+    port: 3000,
+  },
+}
