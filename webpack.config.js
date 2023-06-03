d226070c36b6042fb8c3d9bdad2c121bf425bba8
Whittun <whittun@gmail.com>
2023-06-03 17:23:02 +0300

    feat: add settings for dev-server and html processing

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,44 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: "./src/index.js",
+  entry: "./src/scripts/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js"
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+  ],
+  optimization: {
+    minimize: true,
+  },
   devServer: {
     compress: true,
     port: 3000,
+    static: {
+      directory: __dirname,
+    },
   },
 }
