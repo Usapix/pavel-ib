970be81163b780f222c8b23ae80341f43ac06679
Whittun <whittun@gmail.com>
2023-09-07 21:07:09 +0300

    feat: add responsive layout of the howhelp section

--- src/sections/HowHelp/HowHelp.js
+++ src/sections/HowHelp/HowHelp.js
@@ -2,17 +2,21 @@ import React from "react";
 
 function HowHelp() {
   return (
-    <section className="how-help">
+    <section className="how-help section-wrap">
       <div className="container">
-        <h2 className="how-help__title title">
-          Помогаю крупным <span className="break-line">и малым предприятиям</span>
-        </h2>
-        <p className="how-help__tools-text">
-          Использую в работе:
-        </p>
-        <a className="how-help__tool alt-button" href="index.html">Белый ящик</a>
-        <a className="how-help__tool alt-button" href="index.html">Черный ящик</a>
-        <a className="how-help__tool alt-button" href="index.html">Серый ящик</a>
+        <div className="how-help__wrapper"> 
+          <h2 className="how-help__title title">
+            Помогаю крупным <span className="break-line">и малым предприятиям</span>
+          </h2>
+          <div className="how-help__tools-wrapper">
+            <p className="how-help__tools-text">
+              Использую<span> в работе</span>:
+            </p>
+            <a className="how-help__tool alt-button" href="index.html">Белый ящик</a>
+            <a className="how-help__tool alt-button" href="index.html">Черный ящик</a>
+            <a className="how-help__tool alt-button" href="index.html">Серый ящик</a>
+          </div>
+        </div>
         <ul className="how-help__list description-card-list">
           <li className="how-help__item description-card-list__item">
             <p className="how-help__item-text description-card-list__text">
