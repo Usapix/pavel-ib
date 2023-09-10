45597b5e8548306e948844048783ef1975d18599
Whittun <whittun@gmail.com>
2023-09-10 16:12:18 +0300

    feat: add responsive layout of testing and feedback sections

--- src/sections/Feedback/Feedback.js
+++ src/sections/Feedback/Feedback.js
@@ -4,21 +4,27 @@ function Feedback() {
   return (
     <section className="feedback-form">
       <div className="container">
-        <h2 className="feedback-form__title title">
-          Давайте обезопасим ваш продукт вместе!
-        </h2>
-        <form className="feedback-form__form" action="" method="post">
-          <p className="feedback-form-input-wrapper main-input">
-            <input className="feedback-form__input main-input__element" placeholder="Ваше имя" type="text" name="name" id="feedback-name-field"/>
-          </p>  
-          <p className="feedback-form-input-wrapper main-input">
-            <input className="feedback-form__input main-input__element" placeholder="Ваша почта" type="email" name="email" id="feedback-email-field"/>
-          </p>
-          <button className="feedback-form__button button" type="submit">Давайте</button>
-        </form>
-        <p className="feedback-form__consent">
-          Нажимая на кнопку, вы даёте согласие на обработку персональных данных
-        </p>
+        <div className="feedback-form__wrapper">
+          <h2 className="feedback-form__title title">
+            Давайте обезопасим <span className="break-line">ваш продукт вместе!</span>
+          </h2>
+          <form className="feedback-form__form" action="" method="post">
+            <div className="feedback-form__inputs-wrapper">
+              <p className="feedback-form-input-wrapper main-input">
+                <input className="feedback-form__input main-input__element" placeholder="Ваше имя" type="text" name="name" id="feedback-name-field"/>
+              </p>  
+              <p className="feedback-form-input-wrapper main-input">
+                <input className="feedback-form__input main-input__element" placeholder="Ваша почта" type="email" name="email" id="feedback-email-field"/>
+              </p>
+            </div>
+            <div className="feedback-form__button-wrapper">
+              <button className="feedback-form__button button" type="submit">Давайте</button>
+              <p className="feedback-form__consent">
+                Нажимая на кнопку, вы даёте согласие на обработку персональных данных
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   )
--- src/sections/Testing/Testing.js
+++ src/sections/Testing/Testing.js
@@ -2,35 +2,79 @@ import React from "react";
 
 function Testing() {
   return (
-    <section className="testing">
+    <section className="testing section-wrap">
       <div className="container">
-        <h2 className="testing__title title">
-          Осуществляю услуги <span className="break-line">по тестированию</span>
-        </h2>
-        <svg className="testing__glare" width="169" height="255" viewBox="0 0 169 255" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g filter="url(#filter0_f_468_679)">
-          <ellipse cx="40.7815" cy="78.4741" rx="40.7815" ry="78.4741" transform="matrix(0.903474 -0.428643 0.571613 0.820523 56.6992 -0.0126953)" fill="#3BCC7C" fillOpacity="0.4"/>
-          </g>
-          <path d="M262.367 187.708C259.714 205.724 248.903 213.726 233.456 211.964C218.015 210.204 197.997 198.684 177.067 177.791C156.137 156.899 138.292 130.623 126.357 105.057C114.418 79.4808 108.433 54.7118 111.086 36.6965C113.739 18.6813 124.551 10.679 139.998 12.4404C155.438 14.2012 175.457 25.721 196.387 46.6136C217.317 67.5062 235.162 93.7822 247.096 119.348C259.036 144.924 265.021 169.693 262.367 187.708Z" stroke="white"/>
-          <path d="M287.946 187.708C285.292 205.724 274.481 213.726 259.034 211.964C243.593 210.204 223.575 198.684 202.645 177.791C181.715 156.899 163.87 130.623 151.935 105.057C139.996 79.4808 134.011 54.7118 136.664 36.6965C139.317 18.6813 150.129 10.679 165.576 12.4404C181.017 14.2012 201.035 25.721 221.965 46.6136C242.895 67.5062 260.74 93.7822 272.675 119.348C284.614 144.924 290.599 169.693 287.946 187.708Z" stroke="white"/>
-          <defs>
-          <filter id="filter0_f_468_679" x="0.349609" y="-99.8408" width="276.102" height="293.475" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-          <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-          <feGaussianBlur stdDeviation="40" result="effect1_foregroundBlur_468_679"/>
-          </filter>
-          </defs>
-        </svg>
-        <div className="testing__list">
-          <div className="testing__up-wrapper">
-            <a className="testing__service alt-button">Инфраструктуры</a>
-            <a className="testing__service alt-button">Веб-сайтов</a>
-            <a className="testing__service alt-button">Мобильных приложений</a>
+        <div className="testing__wrapper">
+          <h2 className="testing__title title">
+            Осуществляю услуги <span className="break-line">по тестированию</span>
+          </h2>
+          <svg className="testing__glare testing__glare--320" width="169" height="255" viewBox="0 0 169 255" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_468_679)">
+            <ellipse cx="40.7815" cy="78.4741" rx="40.7815" ry="78.4741" transform="matrix(0.903474 -0.428643 0.571613 0.820523 56.6992 -0.0126953)" fill="#3BCC7C" fillOpacity="0.4"/>
+            </g>
+            <path d="M262.367 187.708C259.714 205.724 248.903 213.726 233.456 211.964C218.015 210.204 197.997 198.684 177.067 177.791C156.137 156.899 138.292 130.623 126.357 105.057C114.418 79.4808 108.433 54.7118 111.086 36.6965C113.739 18.6813 124.551 10.679 139.998 12.4404C155.438 14.2012 175.457 25.721 196.387 46.6136C217.317 67.5062 235.162 93.7822 247.096 119.348C259.036 144.924 265.021 169.693 262.367 187.708Z" stroke="white"/>
+            <path d="M287.946 187.708C285.292 205.724 274.481 213.726 259.034 211.964C243.593 210.204 223.575 198.684 202.645 177.791C181.715 156.899 163.87 130.623 151.935 105.057C139.996 79.4808 134.011 54.7118 136.664 36.6965C139.317 18.6813 150.129 10.679 165.576 12.4404C181.017 14.2012 201.035 25.721 221.965 46.6136C242.895 67.5062 260.74 93.7822 272.675 119.348C284.614 144.924 290.599 169.693 287.946 187.708Z" stroke="white"/>
+            <defs>
+            <filter id="filter0_f_468_679" x="0.349609" y="-99.8408" width="276.102" height="293.475" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feGaussianBlur stdDeviation="40" result="effect1_foregroundBlur_468_679"/>
+            </filter>
+            </defs>
+          </svg>
+          <svg className="testing__glare testing__glare--740" width="292" height="618" viewBox="0 0 292 618" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M463.26 450.896C458.032 496.722 436.745 516.94 406.491 512.487C376.239 508.034 337.047 478.908 296.091 426.134C255.135 373.36 220.195 306.965 196.812 242.305C173.428 177.643 161.62 114.781 166.849 68.9555C172.077 23.1302 193.364 2.91187 223.618 7.36519C253.871 11.8183 293.062 40.9438 334.018 93.7177C374.974 146.492 409.914 212.887 433.297 277.547C456.682 342.209 468.489 405.071 463.26 450.896Z" stroke="white"/>
+            <path d="M513.249 450.896C508.02 496.722 486.734 516.94 456.48 512.487C426.227 508.034 387.035 478.908 346.079 426.134C305.123 373.36 270.184 306.965 246.8 242.305C223.416 177.643 211.608 114.781 216.837 68.9555C222.065 23.1302 243.352 2.91187 273.606 7.36519C303.859 11.8183 343.05 40.9438 384.007 93.7177C424.963 146.492 459.902 212.887 483.285 277.547C506.67 342.209 518.477 405.071 513.249 450.896Z" stroke="white"/>
+            <g filter="url(#filter0_f_445_1355)">
+            <ellipse cx="95" cy="190" rx="95" ry="190" transform="matrix(0.70503 0.709178 -0.121886 0.992544 217.125 -77.4141)" fill="#3BCC7C" fill-opacity="0.4"/>
+            </g>
+            <defs>
+            <filter id="filter0_f_445_1355" x="0.0566406" y="-211.77" width="521.775" height="780.622" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feGaussianBlur stdDeviation="95" result="effect1_foregroundBlur_445_1355"/>
+            </filter>
+            </defs>
+          </svg>
+          <svg className="testing__glare testing__glare--1440" width="416" height="462" viewBox="0 0 416 462" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M710.71 262.093C706.255 275.282 694.944 284.84 678.172 290.777C661.402 296.715 639.232 299.009 613.186 297.714C561.097 295.124 493.616 278.182 423.1 247.486C352.583 216.79 292.424 178.169 252.163 140.559C232.03 121.753 216.895 103.219 207.92 86.075C198.945 68.9295 196.161 53.2367 200.616 40.0473C205.071 26.8579 216.382 17.3007 233.153 11.3629C249.923 5.42564 272.094 3.13091 298.14 4.42611C350.229 7.01631 417.71 23.9581 488.226 54.6542C558.743 85.3502 618.902 123.971 659.163 161.581C679.295 180.387 694.431 198.921 703.405 216.065C712.381 233.211 715.164 248.904 710.71 262.093Z" stroke="white"/>
+            <g filter="url(#filter0_f_244_535)">
+            <ellipse cx="124.611" cy="116.924" rx="124.611" ry="116.924" transform="matrix(0.946827 0.321744 -0.341636 0.939832 287.184 -44.9932)" fill="#3BCC7C" fill-opacity="0.4"/>
+            </g>
+            <path d="M796.71 262.093C792.255 275.282 780.944 284.84 764.172 290.777C747.402 296.715 725.232 299.009 699.186 297.714C647.097 295.124 579.616 278.182 509.1 247.486C438.583 216.79 378.424 178.169 338.163 140.559C318.03 121.753 302.895 103.219 293.92 86.075C284.945 68.9295 282.161 53.2367 286.616 40.0473C291.071 26.8579 302.382 17.3007 319.153 11.3629C335.923 5.42564 358.094 3.13091 384.14 4.42611C436.229 7.01631 503.71 23.9581 574.226 54.6542C644.743 85.3502 704.902 123.971 745.163 161.581C765.295 180.387 780.431 198.921 789.405 216.065C798.381 233.211 801.164 248.904 796.71 262.093Z" stroke="white"/>
+            <defs>
+            <filter id="filter0_f_244_535" x="0.625" y="-252.018" width="729.197" height="714.013" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feGaussianBlur stdDeviation="120" result="effect1_foregroundBlur_244_535"/>
+            </filter>
+            </defs>
+          </svg>
+          <div className="testing__list testing__list--320">
+            <div className="testing__up-wrapper">
+              <a className="testing__service alt-button">Инфраструктуры</a>
+              <a className="testing__service alt-button">Веб-сайтов</a>
+              <a className="testing__service alt-button">Мобильных приложений</a>
+            </div>
+            <div className="testing__bottom-wrapper">
+              <a className="testing__service alt-button">Беспроводных сетей</a>
+              <a className="testing__service alt-button">Сотрудников</a>
+              <a className="testing__service alt-button">Информационных систем</a>
+            </div>
           </div>
-          <div className="testing__bottom-wrapper">
-            <a className="testing__service alt-button">Беспроводных сетей</a>
-            <a className="testing__service alt-button">Сотрудников</a>
-            <a className="testing__service alt-button">Информационных систем</a>
+          <div className="testing__list--740">
+            <div className="testing__wrapper--1">
+              <a className="testing__service alt-button">Веб-сайтов</a>
+              <a className="testing__service alt-button">Мобильных приложений</a>
+            </div>
+            <div className="testing__wrapper--2">
+              <a className="testing__service alt-button">Сотрудников</a>
+              <a className="testing__service alt-button">Инфраструктуры</a>
+            </div>
+            <div className="testing__wrapper--3">
+              <a className="testing__service alt-button">Беспроводных сетей</a>
+              <a className="testing__service alt-button">Информационных систем</a>
+            </div>
           </div>
         </div>
       </div>
