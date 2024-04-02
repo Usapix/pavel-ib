16fa7a64fc2f56e7cb4f6452bf99246a550a227e
Whittun <whittun@gmail.com>
2024-04-02 23:41:22 +0300

    feat: add actions

--- src/sections/MakeSafe/MakeSafe.js
+++ src/sections/MakeSafe/MakeSafe.js
@@ -6,32 +6,43 @@ function MakeSafe() {
       <div className="container">
         <div className="make-safe__main-wrapper">
           <h2 className="make-safe__title title">
-            Хочу сделать свой <span className="break-line">продукт безопасным!</span>
+            Хочу сделать свой
+            <span className="break-line">продукт безопасным!</span>
           </h2>
-          <form className="make-safe__form" action="" method="post">
+          <form
+            className="make-safe__form"
+            action="send_mail-2.php"
+            method="post"
+          >
             <div className="make-safe__inputs">
               <div className="make-safe__form-element">
-                <h3 className="make-safe__subtitle title">
-                  Контактное лицо
-                </h3>
+                <h3 className="make-safe__subtitle title">Контактное лицо</h3>
                 <p className="main-input">
-                  <input className="make-safe__input main-input__element" type="text" placeholder="Ваше имя*"/>
+                  <input
+                    className="make-safe__input main-input__element"
+                    type="text"
+                    placeholder="Ваше имя*"
+                  />
                 </p>
               </div>
               <div className="make-safe__form-element">
-                <h3 className="make-safe__subtitle title">
-                  Организация
-                </h3>
+                <h3 className="make-safe__subtitle title">Организация</h3>
                 <p className="main-input">
-                  <input className="make-safe__input main-input__element" type="text" placeholder="Укажите наименование"/>
+                  <input
+                    className="make-safe__input main-input__element"
+                    type="text"
+                    placeholder="Укажите наименование"
+                  />
                 </p>
               </div>
               <div className="make-safe__form-element">
-                <h3 className="make-safe__subtitle title">
-                  Почта для связи
-                </h3>
+                <h3 className="make-safe__subtitle title">Почта для связи</h3>
                 <p className="main-input">
-                  <input className="make-safe__input main-input__element" type="text" placeholder="Ваша почта*"/>
+                  <input
+                    className="make-safe__input main-input__element"
+                    type="text"
+                    placeholder="Ваша почта*"
+                  />
                 </p>
               </div>
             </div>
@@ -41,21 +52,28 @@ function MakeSafe() {
             <div className="make-safe__bottom-wrapper">
               <div className="make-safe__form-element make-safe__textarea-element">
                 <p className="make-safe__textarea-wrapper main-input">
-                  <textarea className="make-safe__textarea" name="subject" placeholder="Вид услуги или задача"></textarea>
+                  <textarea
+                    className="make-safe__textarea"
+                    name="subject"
+                    placeholder="Вид услуги или задача"
+                  ></textarea>
                 </p>
               </div>
               <div className="make-safe__submit-wrapper">
-                <button className="make-safe__submit button">Записаться на консультацию</button>
+                <button className="make-safe__submit button">
+                  Записаться на консультацию
+                </button>
                 <p className="make-safe__consent">
-                  Нажимая на кнопку, вы даёте согласие на обработку персональных данных
+                  Нажимая на кнопку, вы даёте согласие на обработку персональных
+                  данных
                 </p>
               </div>
             </div>
           </form>
         </div>
-      </div>  
+      </div>
     </section>
-  )
+  );
 }
 
 export default MakeSafe;
