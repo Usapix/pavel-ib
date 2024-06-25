174be73678b8313dee5076b9136c99be635fdbea
Whittun <whittun@gmail.com>
2024-06-25 17:17:25 +0300

    feat: add dynamic date output

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+function getDate() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
 function Footer() {
   return (
     <footer className="main-footer">
@@ -8,41 +13,61 @@ function Footer() {
           <nav className="main-footer__nav">
             <ul className="main-footer__nav-list">
               <li className="main-footer__nav-item">
-                <a href="#testing" className="main-footer__nav-link">Услуги</a>
+                <a href="#testing" className="main-footer__nav-link">
+                  Услуги
+                </a>
               </li>
               <li className="main-footer__nav-item">
-                <a href="#about" className="main-footer__nav-link">Обо мне</a>
+                <a href="#about" className="main-footer__nav-link">
+                  Обо мне
+                </a>
               </li>
               <li className="main-footer__nav-item">
-                <a href="#contacts" className="main-footer__nav-link">Контакты</a>
+                <a href="#contacts" className="main-footer__nav-link">
+                  Контакты
+                </a>
               </li>
               <li className="main-footer__nav-item">
-                <a href="#questions" className="main-footer__nav-link">Вопросы</a>
+                <a href="#questions" className="main-footer__nav-link">
+                  Вопросы
+                </a>
               </li>
             </ul>
           </nav>
           <div className="main-footer__content-wrapper">
             <div className="main-footer__privacy-copy-wrapper">
               <p className="main-footer__copy">
-                ©2024. Pavel|IB — все права защищены
+                ©{getDate()}. Pavel|IB — все права защищены
               </p>
               <p className="main-footer__privacy-policy">
-                <a className="main-footer__privacy-link" href="">Политика конфиденциальности</a>
+                <a className="main-footer__privacy-link">
+                  Политика конфиденциальности
+                </a>
               </p>
             </div>
             <ul className="main-footer__creators-list">
               <li className="main-footer__creators-item">
-                <a href="https://whittun.github.io/rsschool-cv/" className="main-footer__creators-link">Разработчик — Александр Шпанов</a>
+                <a
+                  href="https://whittun.github.io/rsschool-cv/"
+                  className="main-footer__creators-link"
+                >
+                  Разработчик — Александр Шпанов
+                </a>
               </li>
               <li className="main-footer__creators-item">
-                <a href="https://www.behance.net/57de399e" className="main-footer__creators-link">Дизайнер — Анастасия Гудечек</a>
+                <a
+                  href="https://www.behance.net/57de399e"
+                  className="main-footer__creators-link"
+                >
+                  Дизайнер — Анастасия Гудечек
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
