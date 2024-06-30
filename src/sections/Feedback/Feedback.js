b3e9afa864ed95dcf4081bc670055ee8772f9739
Whittun <whittun@gmail.com>
2024-06-30 15:56:11 +0300

    feat: add validation for up form

--- src/sections/Feedback/Feedback.js
+++ src/sections/Feedback/Feedback.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Feedback() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [nameDirty, setNameDirty] = useState(false);
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [nameError, setNameError] = useState("Введите имя");
+  const [emailError, setEmailError] = useState("Введите почту");
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (nameError || emailError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, nameError]);
+
+  const nameHandler = (e) => {
+    setName(e.target.value);
+    const nameRegex = /^(?!.*\s{2,})(?!^\s)(?!.*\s$)[a-zA-Zа-яА-ЯёЁ'\- ]+$/;
+    if (!nameRegex.test(e.target.value)) {
+      setNameError("Некорректное имя");
+    } else {
+      setNameError("");
+    }
+  };
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(e.target.value)) {
+      setEmailError("Некорректный Email");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setEmailDirty(true);
+        break;
+      case "name":
+        setNameDirty(true);
+        break;
+      default:
+    }
+  };
+
   return (
     <section className="feedback-form">
       <div className="container">
@@ -15,27 +64,47 @@ function Feedback() {
             method="post"
           >
             <div className="feedback-form__inputs-wrapper">
-              <p className="feedback-form-input-wrapper main-input">
+              <p
+                className={`feedback-form-input-wrapper main-input ${
+                  nameDirty && nameError ? "main-input--error" : ""
+                }`}
+              >
                 <input
+                  onBlur={(e) => blurHandler(e)}
+                  onChange={(e) => nameHandler(e)}
                   className="feedback-form__input main-input__element"
+                  value={name}
                   placeholder="Ваше имя"
                   type="text"
                   name="name"
                   id="feedback-name-field"
                 />
+                {nameDirty && nameError ? <p>{nameError}</p> : ""}
               </p>
-              <p className="feedback-form-input-wrapper main-input">
+              <p
+                className={`feedback-form-input-wrapper main-input ${
+                  emailDirty && emailError ? "main-input--error" : ""
+                }`}
+              >
                 <input
+                  onBlur={(e) => blurHandler(e)}
+                  onChange={(e) => emailHandler(e)}
                   className="feedback-form__input main-input__element"
+                  value={email}
                   placeholder="Ваша почта"
                   type="email"
                   name="email"
                   id="feedback-email-field"
                 />
+                {emailDirty && emailError ? <p>{emailError}</p> : ""}
               </p>
             </div>
             <div className="feedback-form__button-wrapper">
-              <button className="feedback-form__button button" type="submit">
+              <button
+                className="feedback-form__button button"
+                type="submit"
+                disabled={!formValid}
+              >
                 Давайте
               </button>
               <p className="feedback-form__consent">
